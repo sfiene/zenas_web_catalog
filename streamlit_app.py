5cb105284cbc2be15a84e25495a73fa30dd9eb0d
Scott <52872516+sfiene@users.noreply.github.com>
2023-08-30 10:25:14 -0400

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 # put the dafta into a dataframe
-df = pandas.DataFrame(my_catalog)
+df = pd.DataFrame(my_catalog)
 # temp write the dataframe to the page so I Can see what I am working with
 # streamlit.write(df)
 
